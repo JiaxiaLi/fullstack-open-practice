ab54719ecc006d29f4a7f639a4038eee53d5b4ee
yeyeye <>
2023-06-08 23:10:23 +0800

    part1-unicafe

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lijiaxia
+ * @Date: 2023-06-08 20:45:44
+ * @Email: lijiaxia@3ncto.com
+ * @FilePath: /part1/unicafe/src/index.js
+ * @LastEditors: lijiaxia
+ * @LastEditTime: 2023-06-08 20:46:23
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
