a22079538f60f932c9aea986146ea1d015b18a81
yeyeye <>
2023-06-08 23:35:21 +0800

    part01-courseinfo

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: lijiaxia
+ * @Date: 2023-05-30 19:47:55
+ * @Email: lijiaxia@3ncto.com
+ * @FilePath: /part1/unicafe/src/index.js
+ * @LastEditors: lijiaxia
+ * @LastEditTime: 2023-05-30 19:50:38
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
